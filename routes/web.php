30098a75d5a3155fd06cd8db7cd73c1757880ca9
akbarginanjar <akbar_195@smkassalaambandung.sch.id>
2021-08-04 15:03:48 +0700

    mengubah file

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
 });
+
+Route::get('/about', function () {
+    return view ('about' , [
+        "name" => "Akbar Ginanjar",
+        "email" => "akbarginanjar0@gmail.com",
+        "image" => "akbar.jpeg"
+    ]);
+});
+
+Route::get('/posts', function () {
+    return view ('blog');
+});
+
